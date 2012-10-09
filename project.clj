86717ded0117dd2d0590de199b459a2efc875ea7
Michael Drogalis <mjd3089@rit.edu>
2012-10-09 10:49:12 -0400

    Description and URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hiccup-pipeline "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Instantly funnel CSS and JS files into Clojure web apps."
+  :url "https://github.com/MichaelDrogalis/hiccup-pipeline"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
