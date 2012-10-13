d544fb3946e560987083b7eb4103da83bf8182b8
Michael Drogalis <mjd3089@rit.edu>
2012-10-13 19:10:22 -0400

    Just delete the whole test infrastructure dir in one shot.

--- test/hiccup_pipeline/core_test.clj
+++ test/hiccup_pipeline/core_test.clj
@@ -12,12 +12,8 @@
   (fs/mkdir (str root File/separator "baz"))
   (fs/touch (str root File/separator "baz" File/separator (str "bat" extension))))
 
-(defn tear-down-directory-structure [root extension]
-  (fs/delete (str root File/separator "baz" File/separator (str "bat" extension)))
-  (fs/delete (str root File/separator "baz"))
-  (fs/delete (str root File/separator (str "bar" extension)))
-  (fs/delete (str root File/separator (str "foo" extension)))
-  (fs/delete root))
+(defn tear-down-directory-structure [root]
+  (fs/delete-dir root))
 
 (deftest include-all-css-test
   (set-up-directory-structure "css" ".css")
@@ -25,7 +21,7 @@
          #{(list [:link {:type "text/css", :href (URI. "css/foo.css"), :rel "stylesheet"}])
            (list [:link {:type "text/css", :href (URI. "css/bar.css"), :rel "stylesheet"}])
            (list [:link {:type "text/css", :href (URI. "css/baz/bat.css"), :rel "stylesheet"}])}))
-  (tear-down-directory-structure "css" ".css"))
+  (tear-down-directory-structure "css"))
 
 (deftest include-all-js-test
   (set-up-directory-structure "js" ".js")
@@ -33,4 +29,4 @@
          #{(list [:script {:type "text/javascript", :src (URI. "js/foo.js")}])
            (list [:script {:type "text/javascript", :src (URI. "js/bar.js")}])
            (list [:script {:type "text/javascript", :src (URI. "js/baz/bat.js")}])}))
-  (tear-down-directory-structure "js" ".js"))
+  (tear-down-directory-structure "js"))
